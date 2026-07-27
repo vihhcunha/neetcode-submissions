0ed3afaf4c6ec620ea9283bf392f17e43f8d19ca
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-07-27 11:40:02 -0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const mapA = new Map<string, string[]>();
+        for (let i = 0; i <= strs.length - 1; i++){
+            var orderedWord = strs[i].split('').sort().toString();
+            var wordList = mapA.get(orderedWord) ?? [];
+            wordList.push(strs[i]);
+            mapA.set(orderedWord, wordList);
+        }
+        return [...mapA.values()];
+    }
+}
