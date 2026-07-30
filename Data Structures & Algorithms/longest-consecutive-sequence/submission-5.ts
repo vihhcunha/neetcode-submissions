856e18c009706e7bc7d8b6296a12ad11c8d0b3d7
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-07-30 20:29:12 -0300

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums: number[]): number {
+        let result = 0;
+        let set = new Set<number>();
+
+        for (let i = 0; i <= nums.length - 1; i++){
+            set.add(nums[i]);
+        }
+        
+        for (let num of set){
+            if (set.has(num - 1))
+                continue;
+
+            var lastValue = num;
+            var length = 1;
+            while (set.has(lastValue + 1)){
+                lastValue++;
+                length++;
+                continue;
+            }
+            result = Math.max(length, result);
+            
+        }
+        return result;
+    }
+}
