1fdd53c195f1cbdbd4bfdae4c85a4ee2a4876bc0
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-07-31 12:16:31 -0300

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        const result: number[] = Array(temperatures.length).fill(0);
+        const pendingStack: number[] = [];
+
+        if (temperatures.length == 1)
+            return [0];
+
+        for (var i = 0; i <= temperatures.length - 1; i++){
+            while (temperatures[i] > temperatures[pendingStack[pendingStack.length - 1]]){
+                let index = pendingStack.pop();
+                result[index] = i - index;
+            }
+
+            pendingStack.push(i);
+        }
+        return result;
+    }
+}
