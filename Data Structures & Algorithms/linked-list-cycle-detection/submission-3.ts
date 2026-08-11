a3bd6536c73222a3e7eb8c306d532b568cb53411
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-08-11 11:42:32 -0300

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head: ListNode | null): boolean {
+        let fast = head;
+        let slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next?.next;
+            slow = slow.next;
+
+            if (fast?.val == slow?.val)
+                return true;
+        }
+
+        return false;
+    }
+}
