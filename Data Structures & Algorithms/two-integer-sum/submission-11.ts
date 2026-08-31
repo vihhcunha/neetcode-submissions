61e9957a301dfbf7b9375949956581f5cf5bcd57
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-08-31 07:14:35 -0300

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const map = new Map<number, number>();
+
+        for (let i = 0; i <= nums.length - 1; i++) {
+            let remaining = target - nums[i];
+
+            let j = map.get(remaining);
+            if (j != null) {
+                return [j, i];
+            }
+            map.set(nums[i], i);
+        }
+        return [];
+    }
+}
