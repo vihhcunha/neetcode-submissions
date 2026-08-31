f7556c28222137d4148886dc4965a96ec3f3aa3f
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-08-31 07:16:51 -0300

    Add: palindrome-number - submission-7

---
+++ Data Structures & Algorithms/palindrome-number/submission-7.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} x
+     * @return {boolean}
+     */
+    isPalindrome(x: number): boolean {
+        if (x < 0) {
+            return false;
+        }
+
+        let div = 1;
+        while (x >= div * 10){
+            div = div * 10;
+        }
+
+        while (x != 0) {
+            let firstDigit = Math.floor(x / div);
+            let lastDigit = Math.floor(x % 10);
+
+            if (firstDigit !== lastDigit) {
+                return false;
+            }
+
+            x = Math.floor((x % div) / 10);
+            div = div / 100;
+        }
+        return true;
+    }
+}
