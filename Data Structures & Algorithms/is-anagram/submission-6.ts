763aecb87ff3faf6e06c078329885d25c0116773
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-09-02 08:37:04 -0300

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const charMapS = new Map<string, number>();
+        const charMapT = new Map<string, number>();
+
+        if (s.length != t.length) {
+            return false;
+        }
+
+        for (let i = 0; i <= s.length - 1; i++) {
+            let charCountS = (charMapS.get(s[i]) ?? 0) + 1;
+            charMapS.set(s[i], charCountS);
+
+            let charCountT = (charMapT.get(t[i]) ?? 0) + 1;
+            charMapT.set(t[i], charCountT);
+        }
+
+        for (let [key, countS] of charMapS.entries()) {
+            if (charMapT.get(key) != countS){
+                return false;
+            }
+        }
+        return true;
+    }
+}
