9d7bce2c545d72e06443923233ea462cc8f34636
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-09-07 20:32:06 -0300

    Add: balanced-binary-tree - submission-4

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-4.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root: TreeNode | null): boolean {
+        return this.dfs(root)[0] === 1;
+    }
+
+    dfs(root: TreeNode) {
+        if (!root) {
+            return [1, 0];
+        }
+
+        const left = this.dfs(root.left);
+        const right = this.dfs(root.right);
+
+        const balanced =
+            left[0] === 1 &&
+            right[0] === 1 &&
+            Math.abs(left[1] - right[1]) <= 1;
+        const height = 1 + Math.max(left[1], right[1]);
+
+        return [balanced ? 1 : 0, height];
+    }
+}
