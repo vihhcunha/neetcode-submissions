4669ef28e7412f6bda168467535cc68b7a2b8d0b
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-09-08 17:46:20 -0300

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    postorderTraversal(root: TreeNode | null): number[] {
+        if (root == null) {
+            return [];
+        }
+        const result = [];
+        result.push(...this.postorderTraversal(root.left));
+        result.push(...this.postorderTraversal(root.right));
+        result.push(root.val);
+        return result;
+    }
+}
