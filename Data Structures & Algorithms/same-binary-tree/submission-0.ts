40771af677c90a896a3973e07ecc0307ff9746e3
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2026-09-09 06:12:30 -0300

    Add: same-binary-tree - submission-0

---
+++ Data Structures & Algorithms/same-binary-tree/submission-0.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+        if (p == null && q == null) {
+            return true;
+        }
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        let rRes = this.isSameTree(p.right, q.right);
+        let lRes = this.isSameTree(p.left, q.left);
+        return rRes && lRes;
+    }
+}
